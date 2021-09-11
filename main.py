9e824f5081dd0f423301cfc15ed138b6dad3e3c3
jh <kojh0111@naver.com>
2021-09-11 19:05:28 +0900

    Countries Table

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
+from data import countries_df
+from builders import make_table
 
 stylesheets = [
     "https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css",
@@ -21,7 +23,8 @@ app.layout = html.Div(
         html.Header(
             style={"textAlign": "center", "paddingTop": "50px"},
             children=[html.H1("Corona Dashboard", style={"fontSize": 40})],
-        )
+        ),
+        html.Div(children=[html.Div(children=[make_table(countries_df)])]),
     ],
 )
 
