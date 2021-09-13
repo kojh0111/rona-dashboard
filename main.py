69de05bff21cdb3aa0295d0493bee22f36b98f28
jh <kojh0111@naver.com>
2021-09-13 11:09:38 +0900

    Updating the Graph by Country

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
-from data import countries_df, totals_df, dropdown_options
+from data import (
+    countries_df,
+    make_country_df,
+    make_global_df,
+    totals_df,
+    dropdown_options,
+)
 from builders import make_table
 
 stylesheets = [
@@ -82,6 +88,7 @@ app.layout = html.Div(
             children=[
                 html.Div(children=[dcc.Graph(figure=bars_graph)]),
                 html.Div(
+                    style={"grid-column": "span 3"},
                     children=[
                         dcc.Dropdown(
                             placeholder="Choose Country",
@@ -91,8 +98,8 @@ app.layout = html.Div(
                                 for country in dropdown_options
                             ],
                         ),
-                        html.H1(id="country-select"),
-                    ]
+                        dcc.Graph(id="country_graph"),
+                    ],
                 ),
             ],
         ),
@@ -100,9 +107,22 @@ app.layout = html.Div(
 )
 
 
-@app.callback(Output("country-select", "children"), [Input("country", "value")])
+@app.callback(Output("country_graph", "figure"), [Input("country", "value")])
 def update_hello(value):
-    print(value)
+    df = make_country_df(value) if value else make_global_df()
+    fig = px.line(
+        df,
+        x="date",
+        y=["confirmed", "deaths", "recovered"],
+        template="plotly_dark",
+        labels={"value": "Cases", "variable": "Condition", "date": "Date"},
+        hover_data={"value": ":,", "variable": False, "date": False},
+    )
+    fig.update_xaxes(rangeslider_visible=True)
+    fig["data"][0]["line"]["color"] = "#e74c3c"
+    fig["data"][1]["line"]["color"] = "#8e44ad"
+    fig["data"][2]["line"]["color"] = "#27ae60"
+    return fig
 
 
 if __name__ == "__main__":
